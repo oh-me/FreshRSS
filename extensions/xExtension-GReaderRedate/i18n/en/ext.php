9feb646e17f0f51eaf25404f6c614a655430b3f8
oh-me <58557633+oh-me@users.noreply.github.com>
2022-08-26 11:35:54 +0400

    Add files via upload

---
+++ extensions/xExtension-GReaderRedate/i18n/en/ext.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+	'greader_redate' => array(
+		'explanation' => 'This extension replace the fetched date of articles used by greader API with the publishing date.',
+		'instruction' => 'Please select at which RSS clients this replacement should be done:',
+		'reeder' => 'Reeder',
+		'easy_rss' => 'EasyRSS',
+		'feedme' => 'FeedMe',
+		'updates' => 'You can find the latest extension version at',
+	),
+);
+
+?>
